6ad2a067d7d9fa9c5b5ad96dedd71965750746ad
Serhii Khoma <srghma@gmail.com>
2019-07-31 22:36:15 +0300

    fix: filter .git dir

--- default.nix
+++ default.nix
@@ -5,10 +5,16 @@
 with pkgs;
 with pkgs.haskell.lib;
 
+let
+additionalIgnores = ''
+  .git
+'';
+in
+
 ((import ./stack2nix.nix { inherit pkgs; }).override {
   overrides = self: super: {
     ntmuxp = justStaticExecutables (overrideCabal super.ntmuxp (old: {
-      src = gitignore.gitignoreSource [./.gitignore] ./.;
+      src = gitignore.gitignoreSource [./.gitignore additionalIgnores] ./.;
 
       buildTools = old.buildTools or [] ++ [ makeWrapper ];
 
